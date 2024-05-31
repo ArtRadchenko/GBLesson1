57e44124638d315b56c74f87acd8eaa32981e47f
Artem Radchenko <sdad@bk.ru>
2024-06-01 01:47:09 +0300

    Создал пример нескольких персон. Описал персону, добавил геттеры и сеттеры. Удалил src

--- familyTree/Main.java
+++ familyTree/Main.java
@@ -1,4 +1,15 @@
 package familyTree;
+
+import java.time.LocalDate;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        FamilyTree familyTree = new FamilyTree();
+
+        // Создадим для примера несколько персон
+        Person john = new Person("John", LocalDate.of(1950, 1, 1), true, "Male");
+        Person jane = new Person("Jane", LocalDate.of(1955, 2, 2), true, "Female");
+        Person child1 = new Person("Child1", LocalDate.of(1980, 3, 3), true, "Male");
+        Person child2 = new Person("Child2", LocalDate.of(1982, 4, 4), true, "Female");
+    }
 }
--- familyTree/Person.java
+++ familyTree/Person.java
@@ -1,4 +1,71 @@
 package familyTree;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person {
-    
+    private String name;
+    private LocalDate birthDate;
+    private boolean isAlive;
+    private LocalDate deathDate;
+    private String gender;
+    private List<Person> children;
+
+    public Person(String name, LocalDate birthDate, boolean isAlive, String gender) {
+        this.name = name;
+        this.birthDate = birthDate;
+        this.isAlive = isAlive;
+        this.gender = gender;
+        this.children = new ArrayList<>();
+    }
+
+    // Геттеры и сеттеры
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean isAlive) {
+        this.isAlive = isAlive;
+    }
+
+    public LocalDate getDeathDate() {
+        return deathDate;
+    }
+
+    public void setDeathDate(LocalDate deathDate) {
+        this.deathDate = deathDate;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public List<Person> getChildren() {
+        return children;
+    }
+
+    public void addChild(Person child) {
+        this.children.add(child);
+    }
 }
